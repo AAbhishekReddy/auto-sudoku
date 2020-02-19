07f4caad5b8beade445aa52549752ff47ac4ce57
AAbhishekReddy <annemabhishekreddy@gmail.com>
2020-02-19 10:19:53 +0530

    video test

--- script.py
+++ script.py
@@ -1,8 +1,26 @@
+import numpy as np 
+import cv2 as cv
 import pytesseract as tess
-from PIL import Image
+from PIL import Image as im 
 
+cap = cv.VideoCapture(0)
 
-img = Image.open('suduko.jpg')
-text = tess.image_to_string(img)
+while True:
+	print("here")
+	ret, frame = cap.read()
+	print("not here")
+	frame = cv.flip(frame, 1)
 
-print(text)
+	# cv.imshow('frame', frame)
+	cv.imwrite('frame.png', frame)
+
+	img = im.read('frame.png')
+	text = tess.image_to_string(img)
+
+	print(frame)
+
+	if cv.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv.destroyAllWindows()
